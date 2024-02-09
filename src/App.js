76853c2b67ab872c2e2df0687aa2706e51c72ad4
1axat <akakshatsharma3@gmail.com>
2024-02-09 22:45:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './component/Header';
+import Team1  from './component/Team1';
+import Team2 from './component/Team2';
+import Team3 from './component/Team3';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Team1 />
+      <Team2 />
+      <Team3 />
+    </div>
+  );
+}
+
+export default App;
